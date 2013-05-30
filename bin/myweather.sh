6fd4c8297f876df3976cf0e6b4c8fe27f190864b
FireElementalNE <FireElemental.NE@gmail.com>
2013-05-30 12:10:40 -0400

    got tired of changing dirs for every new computer

--- bin/myweather.sh
+++ bin/myweather.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 #be sure to change the directories OR add this dir to your path!
+MYDIR=~/terminal-weather/weather.py
 if [ $# -eq 3 ]; then
     UNIT=$3
     CITY=$1
     COUNTRY=$2
-    python ~/weather/weather.py ${CITY} ${COUNTRY} ${UNIT}
+    python ${MYDIR} ${CITY} ${COUNTRY} ${UNIT}
 else
     if [ $# -eq 2 ]; then
 	if [[ $2 =~ \d\d\d\d+ ]]; then
 	    CITY=$1
 	    UNIT=$2
-	    python ~/weather/weather.py ${CITY} ${UNIT}
+	    python ${MYDIR} ${CITY} ${UNIT}
 	else
 	    CITY=$1
 	    COUNTRY=$2
-	    python ~/weather/weather.py ${CITY} ${COUNTRY}
+	    python ${MYDIR} ${CITY} ${COUNTRY}
 	fi
     else
 	if [ $# -eq 1 ]; then
 	    CITY=$1
-	    python ~/weather/weather.py ${CITY}
+	    python ${MYDIR} ${CITY}
 	else
-	    python ~/weather/weather.py
+	    python ${MYDIR}
 	fi
     fi
 fi
